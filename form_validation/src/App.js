b76fb9b11acea3200367600773d99e6d5ea18451
Roaa Albishri <106008427+roaax@users.noreply.github.com>
2022-08-23 15:25:03 +0300

    Form Validation Assignment

---
+++ form_validation/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Validation from './components/Validation';
+
+function App() {
+  return (
+    <div className="App">
+        <Validation />
+    </div>
+  );
+}
+
+export default App;
