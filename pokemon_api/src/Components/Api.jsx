198109130294d0207da6fcd0a321aae7005d7caa
Roaa Albishri <106008427+roaax@users.noreply.github.com>
2022-08-24 15:34:09 +0300

    Pokemon API Assignment

---
+++ pokemon_api/src/Components/Api.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useState } from "react";
+import "../App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Api=()=>{
+    const[state, setState]=useState([]);
+    const add=()=>{
+        fetch('https://pokeapi.co/api/v2/pokemon/?limit=807')
+            .then(response=>{
+                return response.json()
+            }).then(response=>{setState(response.results);
+            })}
+    return(
+        <div className="container shadow-lg ">
+            <button className="btn btn-dark m-4" onClick={add} >Fetch Pokemon</button>
+            {state.map((item,index)=>{
+                return(<div key={index}>
+                    <ul>
+                        <li>
+                            {item.name}
+                        </li>
+                    </ul>
+                </div>)
+            })}
+        </div>
+    )
+}
+
+export default Api;
